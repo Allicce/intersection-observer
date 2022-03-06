85254e5c71a708a399f3110d1a925cb8291226d0
allicce <stetiarova.alzbeta@gmail.com>
2022-03-06 11:36:37 +0100

    chore(intersection observer) add thumbnails to image for lazy loading

--- src/components/imageRenderer/ImageRenderer.js
+++ src/components/imageRenderer/ImageRenderer.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import './ImageRenderStyle.css'
+import {useIntersection} from "../../hooks/useIntersectionObserver";
 
-const ImageRenderer = ({width, height}) =>{
+const ImageRenderer = ({url, thumbnail, width, height}) =>{
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [isInView, setIsInView] = useState(false);
+    const imgRef = useRef();
+
+    useIntersection(imgRef, () => {
+        setIsInView(true)
+    })
+
+    const handleOnLoad = () => {
+        setIsLoaded(true);
+    }
     return (
         <div
             className="image-container"
-            // ref={imgRef}
+            ref={imgRef}
             style={{
                 paddingBottom: `${(height / width) * 100}%`,
                 width: '100%'
             }}
         >
-
+            {isInView &&(
+                <>
+                    <img
+                        className={`image thumb ${isLoaded && 'isLoaded'}`}
+                        src={thumbnail}
+                    />
+                    <img
+                        className={`image ${isLoaded && 'isLoaded'}`}
+                        src={url}
+                        onLoad={handleOnLoad}
+                    />
+                </>
+            )}
         </div>
     )
 }
